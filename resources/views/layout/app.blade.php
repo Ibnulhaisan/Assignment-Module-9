901b2ecd62b2c7f184f1ebe35a4ebc8c7dac9b41
Ibnulhaisan <60622364+Ibnulhaisan@users.noreply.github.com>
2023-11-27 22:21:36 +0600

    assignment-9

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>DevFolio Bootstrap Portfolio Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{asset('bootstrap5-plain-assets/images/favicon.png')}}" rel="icon">
+  <link href="{{asset('bootstrap5-plain-assets/images/apple-touch-icon.png')}}" rel="icon">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+  {{-- {{asset('assets/img/favicon.png')}} --}}
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: DevFolio
+  * Updated: Nov 17 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+@include('layout.header')
+
+
+@include('layout.footer')
+ <!-- Vendor JS Files -->
+ <script src="{{asset('vendor/purecounter/purecounter_vanilla.js')}}"></script>
+ <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+ <script src="{{asset('vendor/glightbox/js/glightbox.min.js')}}"></script>
+ <script src="{{asset('vendor/swiper/swiper-bundle.min.js')}}"></script>
+ <script src="{{asset('vendor/typed.js/typed.umd.js')}}"></script>
+ <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+
+ <!-- Template Main JS File -->
+ <script src="{{asset('js/main.js')}}"></script>
+
+</body>
+
+</html>
